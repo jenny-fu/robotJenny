b1dcf99e99dc5033dec3a1099ddc89cfe92b6cb8
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-10-02 10:44:34 -0400

    Ay watch your mouth

--- RobotJenny/src/chatbotProject/ChatbotSam.java
+++ RobotJenny/src/chatbotProject/ChatbotSam.java
@@ -3,15 +3,21 @@ package chatbotProject;
 public class ChatbotSam implements Topic{
 
 	private String[] keywords;
+	private String[] compliments;
+	private String[] insults;
 	private String goodbyeWord;
 	private String secretWord;
 	private boolean chatting;
 	
 	public ChatbotSam() {
-		String[] temp = {"food", "entertainment", "Internet", "video games"};
+		String[] temp = {"pretty", "beautiful", "gorgeous", "sexy", "ugly", "fat", "disgusting"};
 		keywords = temp;
+		String[] temp2 = {"pretty", "beautiful", "gorgeous", "sexy"};
+		compliments = temp2;
+		String[] temp3 = {"ugly", "fat", "disgusting"};
+		insults = temp3;
 		goodbyeWord = "bye";
-		secretWord = "pugs";
+		secretWord = "exo";
 	}
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length; i++) {
@@ -21,9 +27,25 @@ public class ChatbotSam implements Topic{
 		}
 		return false;
 	}
+	public boolean isTriggeredCompliments(String response) {
+		for(int i = 0; i < compliments.length; i++) {
+			if(ChatbotMain.findKeyword(response, compliments[i], 0) >=0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean isTriggeredInsults(String response) {
+		for(int i = 0; i < insults.length; i++) {
+			if(ChatbotMain.findKeyword(response, insults[i], 0) >=0) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public void startChatting(String response) {
-		ChatbotMain.print("Hey! It sounds like you and i have a common interest! Let's Talk some more");
+		ChatbotMain.print("That's an interesting thing to say to me.");
 		chatting = true;
 		while(chatting) {
 			response = ChatbotMain.getInput();
@@ -33,6 +55,12 @@ public class ChatbotSam implements Topic{
 			}
 			else if(ChatbotMain.findKeyword(response, secretWord, 0)>=0) {
 				ChatbotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now.");	
+			}
+			else if(isTriggeredCompliments(response)) {
+				ChatbotMain.print("Why thank you very much.");
+			}
+			else if(isTriggeredInsults(response)) {
+				ChatbotMain.print("Ay watch your mouth before I smack you.");
 			}
 			else {
 				ChatbotMain.print("Huh. I don't really get you. Tell me something else");
