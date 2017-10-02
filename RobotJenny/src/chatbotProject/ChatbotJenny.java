4b8bf9de73f4230c707149ed3f3f72adc0fbd5b2
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-10-02 11:40:37 -0400

    things

--- RobotJenny/src/chatbotProject/ChatbotJenny.java
+++ RobotJenny/src/chatbotProject/ChatbotJenny.java
@@ -5,15 +5,19 @@ public class ChatbotJenny implements Topic {
 	private String[] keywords;
 	private String[] question;
 	private String[] excuse;
+	private String[] confessions;
 	private String goodbyeWord;
 	private String secretWord;
+	private boolean secretTalk;
 	private boolean chatting;
 
 	public ChatbotJenny() {
-		String[] temp = {"date", "free", "hang out", "dating"}; //girlfriend
+		String[] temp = {"date", "free", "hang out", "dating"};
+		String[] temp2 = {"be my girlfriend", "be your boyfriend", "like you", "love you"};
 		String[] temp3 = {"why not", "why", "why don't", "how come"};
 		String[] temp4 = {"I have a lot of homework today.", "I have work.", "I have some family business I need to go to."};
 		keywords = temp;
+		confessions = temp2;
 		question = temp3;
 		excuse = temp4;
 		goodbyeWord = "bye";
@@ -27,6 +31,7 @@ public class ChatbotJenny implements Topic {
 		}
 		return false;
 	}
+	
 	public boolean questioned(String response) {
 		for(int i = 0; i < question.length; i++) {
 			if(ChatbotMain.findKeyword(response, question[i], 0) >= 0) {
@@ -35,6 +40,16 @@ public class ChatbotJenny implements Topic {
 		}	
 		return false;
 	}
+	
+	public boolean confessed(String response) {
+		for(int i = 0; i < confessions.length; i++) {
+			if(ChatbotMain.findKeyword(response, confessions[i], 0) >= 0) {
+				return true;
+			}
+		}	
+		return false;
+	}
+	
 	public void startChatting(String response) {
 		ChatbotMain.print("No, sorry.");
 		chatting = true;
@@ -44,14 +59,21 @@ public class ChatbotJenny implements Topic {
 				int idx = (int) Math.floor(Math.random() * excuse.length);
 				ChatbotMain.print(excuse[idx]);
 			}else
-				if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
-					chatting = false;
-					ChatbotMain.chatbot.startTalking();
+				if(confessed(response)) {
+		///////////////////////////////////////////////////////////////////
 				}else
-					if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
-						ChatbotMain.print("OMG you guessed my favorite thing ever! Are you free this week?");
+					if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
+						chatting = false;
+						ChatbotMain.chatbot.startTalking();
 					}else
-						ChatbotMain.print("Huh. I don't really get you. Tell me something else.");
+						if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
+							ChatbotMain.print("OMG you that's my favorite thing ever! Are you free this week?");
+							secretTalk = true;
+							while(secretTalk) {
+		///////////////////////////////////////////////////////////////////						
+							}
+						}else
+							ChatbotMain.print("Huh. I don't really get you. Tell me something else.");
 		}
 	}
 
